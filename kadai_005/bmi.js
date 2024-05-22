3d743ba083de1fb3e41660a773bc586d34c019bb
riho-riho <99725441+riho-riho@users.noreply.github.com>
2024-05-22 19:50:31 +0900

    kadai_005を修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,5 +4,11 @@ console.log ('体重：68kg');
 //身長
 console.log('身長：1.7m');
 
+// 変数の宣言
+let bmi;
+
+// 値の代入
+bmi = (68 / [1.7 * 1.7]);
+
 //BMI（体格指数) 計算式：[体重(kg)]÷[身長(m)×身長(m)]
-console.log(68 / [1.7 * 1.7]);
+console.log(bmi);
